568cb96f63b234a3a11b5995eb1f237f1191cfbf
Gabriel M Silva <gmbiel1@gmail.com>
2019-05-07 22:20:17 -0300

    Consertando commit

--- ConveniarEventos/app/src/main/java/com/mthrsj/conveniareventos/MainActivity.java
+++ ConveniarEventos/app/src/main/java/com/mthrsj/conveniareventos/MainActivity.java
@@ -1,6 +1,6 @@
 package com.mthrsj.conveniareventos;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
