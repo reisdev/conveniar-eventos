a3b7659c60c76fa3bd96da158e2cc3922f34b0e3
Matheus Reis <matheusdrdj@gmail.com>
2019-06-10 02:42:30 -0300

    Implementing profile page

--- ConveniarEventos/app/src/main/java/com/mthrsj/conveniareventos/perfil_frag.java
+++ ConveniarEventos/app/src/main/java/com/mthrsj/conveniareventos/perfil_frag.java
@@ -9,8 +9,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class perfil_frag extends Fragment {
+    private String[] options = new String[] {"Meus Dados","Alterar Senha", "Ajuda", "Sair"};
+
     public static perfil_frag newInstance() {
         perfil_frag fragment = new perfil_frag();
         return fragment;
@@ -24,6 +28,11 @@ public class perfil_frag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_perfil_frag, container, false);
+        View inflated = inflater.inflate(R.layout.fragment_perfil_frag, container, false);
+        ArrayAdapter options_adapter = new ArrayAdapter(inflated.getContext(),R.layout.menu_item);
+        options_adapter.addAll(options);
+        ListView options_list  = inflated.findViewById(R.id.menu_list);
+        options_list.setAdapter(options_adapter);
+        return inflated;
     }
 }
